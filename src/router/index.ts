fbbe329d0f4953e8055bb2cd0c6edf1dc6010b00
vibol <vibol.khan@axivit.com>
2026-04-21 17:15:14 +0700

    initial demo project

--- src/router/index.ts
+++ src/router/index.ts
@@ -26,7 +26,11 @@ const routes: Array<RouteRecordRaw> = [
       {
         path: 'tab3',
         component: () => import('@/views/Tab3Page.vue')
-      }
+      },
+      {
+        path: 'tab4',
+        component: () => import('@/views/Tab4Page.vue')
+      },
     ]
   }
 ]
--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -4,18 +4,35 @@
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon aria-hidden="true" :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
+          <ion-icon
+            aria-hidden="true"
+            :icon="tabIcon('/tabs/tab1', grid, gridOutline)"
+          />
+          <ion-label>Home</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon aria-hidden="true" :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
+          <ion-icon
+            aria-hidden="true"
+            :icon="tabIcon('/tabs/tab2', addCircle, addCircleOutline)"
+          />
+          <ion-label>Requests</ion-label>
+        </ion-tab-button>
+        
+        <ion-tab-button tab="tab4" href="/tabs/tab4">
+          <ion-icon
+            aria-hidden="true"
+            :icon="tabIcon('/tabs/tab4', receipt, receiptOutline)"
+          />
+          <ion-label>History</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon aria-hidden="true" :icon="square" />
-          <ion-label>Tab 3</ion-label>
+          <ion-icon
+            aria-hidden="true"
+            :icon="tabIcon('/tabs/tab3', person, personOutline)"
+          />
+          <ion-label>Profile</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
@@ -23,6 +40,32 @@
 </template>
 
 <script setup lang="ts">
-import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import {
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+  IonLabel,
+  IonIcon,
+  IonPage,
+  IonRouterOutlet,
+} from "@ionic/vue";
+import {
+  receipt,
+  receiptOutline,
+  addCircle,
+  addCircleOutline,
+  person,
+  personOutline,
+  grid,
+  gridOutline,
+} from "ionicons/icons";
+import { useRoute } from "vue-router";
+import { p } from "vue-router/dist/router-CWoNjPRp.mjs";
+
+const route = useRoute();
+
+const tabIcon = (path: string, activeIcon: string, inactiveIcon: string) =>
+  route.path === path || route.path.startsWith(`${path}/`)
+    ? activeIcon
+    : inactiveIcon;
 </script>
